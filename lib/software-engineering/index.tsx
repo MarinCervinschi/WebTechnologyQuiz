29f92b7cf588f5e036defe9ed8204b707157b980
MarinCervinschi <marincervinschi3@gmail.com>
2025-01-25 10:56:15 +0100

    feat: create sotware eng. class

---
+++ lib/software-engineering/index.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import QuizSection from "@/types/QuizSection";
+import { shuffleArray } from "../utils";
+
+const softwareEngineering: QuizSection[] = [
+    // insert sections here
+
+];
+
+export default softwareEngineering;
