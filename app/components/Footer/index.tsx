b06e1d2cf8e6e9321001c1ee62988f5862cf301e
MarinCervinschi <marincervinschi3@gmail.com>
2025-02-05 12:44:25 +0100

    ci: footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,11 +1,15 @@
+import { FaGithubAlt } from "react-icons/fa";
+
 export default function Footer() {
-    return (
-      <footer className="text-gray-600 py-4 mt-auto">
-        <div className="container mx-auto text-center">
-          <p>&copy; {new Date().getFullYear()} Quiz App. Created by Marin Cervinschi</p>
-        </div>
-      </footer>
-    )
-  }
-  
-  
+  return (
+    <footer className="text-gray-600 py-4 mt-auto">
+      <div className="container mx-auto text-center overflow-hidden">
+        <p>&copy; {new Date().getFullYear()} Quiz App. Created by <a href="https://github.com/MarinCervinschi/TriviaMore" className="inline-flex items-center space-x-1 text-gray-500 hover:text-gray-600">
+          <span>Marin Cervinschi</span><FaGithubAlt />
+        </a>
+        </p>
+      </div>
+    </footer>
+  )
+}
+
