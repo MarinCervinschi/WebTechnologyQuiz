5d430cc7815041eab4c3e657950103e74285cf6a
MarinCervinschi <marincervinschi3@gmail.com>
2025-02-06 12:48:40 +0100

    fix: build

--- app/components/Loader/index.tsx
+++ app/components/Loader/index.tsx
@@ -1,7 +1,23 @@
-import { grid } from 'ldrs'
-grid.register()
+import { useEffect } from 'react';
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'l-grid': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+                size?: string;
+                speed?: string;
+                color?: string;
+            };
+        }
+    }
+}
 
 const Loader = () => {
+    useEffect(() => {
+        const { grid } = require('ldrs');
+        grid.register();
+    }, []);
+
     return (
         <div className="flex h-screen items-center justify-center bg-white">
             <l-grid
